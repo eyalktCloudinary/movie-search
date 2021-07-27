bfd5e2962c4bf51e21cf99d479b7e9f2ff03eb10
eyalktCloudinary <eyal.kt@cloudinary.com>
2021-07-27 10:43:30 +0300

    add search by enter

--- src/modules/SearchBar.js
+++ src/modules/SearchBar.js
@@ -4,25 +4,30 @@ import searchIcon from '../search.svg';
 // Search component that accepts query and type from the end-user
 function SearchBar({ onSearchGo, types }) {
 
-  const [input, setInput] = useState({}) // consider regulat variable like below
-  // const input = {}
+  const [input, setInput] = useState();
 
   const chooseType = (type) => {
-    console.log("type changes", type);
-    // input.type = type.toLowerCase();
-    setInput({...input, type:type.toLowerCase() });
+    console.log('type changes', type);
+    setInput({...input, type:type.toLowerCase()});
   }
 
-  const handleStrInput = (str) => {
+  const handleStrInput = (str, e) => {
     setInput({...input, str});
+  }
+  
+  const handleEnter = (key) => {
+    if (key !== 'Enter') return; 
+    else if (input.str) onSearchGo(input);
   }
 
   return (
-    <div className="search-bar">
+    <div className="search-bar" onKeyPress={e => handleEnter(e.key)}>
       <div className="search-input">
         <input
+          type="text" 
+          value={input ? input.str : ""}
           placeholder={"Search a movie, series, or episode"}
-          onChange={(e) => handleStrInput(e.target.value)}
+          onChange={e => handleStrInput(e.target.value)}
         />
       </div>
       
@@ -35,7 +40,7 @@ function SearchBar({ onSearchGo, types }) {
       }
 
       <div className="search-btn">
-        <button onClick={(e) => onSearchGo(input)}>
+        <button onClick={() => onSearchGo(input)}>
           <img src={searchIcon} alt='Search button' />
         </button>
       </div>
