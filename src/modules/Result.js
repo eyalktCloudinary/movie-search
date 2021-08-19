7d6ecc6eac6fd23d969f03d9a3876956826a93bd
eyalktCloudinary <eyal.kt@cloudinary.com>
2021-08-19 10:55:06 +0300

    handle n/a poster

--- src/modules/Result.js
+++ src/modules/Result.js
@@ -1,23 +1,30 @@
-import {AdvancedImage} from '@cloudinary/react';
-import {format, quality} from '@cloudinary/base/actions/delivery';
-import {auto} from '@cloudinary/base/qualifiers/format';
-import {auto as qAuto} from '@cloudinary/base/qualifiers/quality';
+import { AdvancedImage } from '@cloudinary/react';
+import { format, quality } from '@cloudinary/base/actions/delivery';
+import { auto } from '@cloudinary/base/qualifiers/format';
+import { auto as qAuto } from '@cloudinary/base/qualifiers/quality';
 import useCloudinary from '../hooks/useCloudinary';
+
+import posterPlaceHolder from '../search.svg';
 
 // represents a single result
 function Result({ movie }) {
 
   const { cld } = useCloudinary();
 
-  const posterImg = cld.image(movie.Poster)
-    .delivery(format(auto()))
-    .delivery(quality(qAuto()))
-    .setStorageType('fetch');
-  
+  const posterImg = movie.Poster === 'N/A' ? posterPlaceHolder :
+    cld.image(movie.Poster)
+      .delivery(format(auto()))
+      .delivery(quality(qAuto()))
+      .setStorageType('fetch');
+
   return (
     <div className="result">
       <div className="result-img">
-       <AdvancedImage cldImg={posterImg} alt={movie.Title + " poster"} />
+        {
+          posterImg === posterPlaceHolder ?
+            <img src={posterPlaceHolder} alt={'placeholder for unavailable "' + movie.Title + '" poster'} /> :
+            <AdvancedImage cldImg={posterImg} alt={'"' + movie.Title + '" poster'} />
+        }
       </div>
       <div className="result-details">
         <h3>{movie.Title}</h3>
